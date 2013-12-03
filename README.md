58d0de588c07875b51a8560e4609c51b45167531
Jeff Mace <jeffmace@gmail.com>
2013-12-03 12:44:17 -0500

    First commit for the ec2_hosts module that builds the /etc/hosts file on EC2 hosts for all servers matching a tag key=>value pair

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-ec2_hosts
-=========
+# ec2_hosts
 
 Write private IP address to /etc/hosts for a set of EC2 servers based on a tag key=>value combination
+
+# Usage
+
+This module uses Facter to load information about matching EC2 servers. Add these values to the facts passed into Puppet.
+
+* "aws_access_key" => "ACCESS_KEY"
+* "aws_secret_access_key" => "SECRET_ACCESS_KEY"
+* "ec2_tag_group_key" => "HostnameGroup"
+* "ec2_tag_group_value" => "DatabasesGroup"
+
+Add the ec2_hosts class to your manifest. You can optionally include the first portion of the hostname in /etc/hosts.
+
+    class { "ec2_hosts": 
+  		include_short_hostname => true
+  	}
---
+++ lib/facter/ec2_tag_group.rb
@@ -0,0 +1,56 @@
+# === Copyright
+#
+# Copyright 2013 Continuent Inc.
+#
+# === License
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+Facter.add(:ec2_tag_group) do
+  setcode do
+    if Facter.value('ec2_tag_group_key').to_s() != ""
+      ENV['AWS_ACCESS_KEY'] = Facter.value('aws_access_key')
+      ENV['AWS_SECRET_KEY'] = Facter.value('aws_secret_access_key')
+    
+      # Fina all instances that have the matching value for the request tag key
+      instances = `ec2-describe-tags --filter "resource-type=instance" --filter "key=#{Facter.value('ec2_tag_group_key')}" --filter "value=#{Facter.value('ec2_tag_group_value')}" | cut -f3`.split("\n")
+      # Get instance details for all matching instances that are running
+      details = `ec2-describe-instances --filter "instance-state-name=running" #{instances.join(' ')}`.split("\n")
+    
+      tag_group = {}
+      details.each{|info|
+        parts = info.split("\t")
+        if parts[0] == "RESERVATION"
+        elsif parts[0] == "INSTANCE"
+          tag_group[parts[1]] = {} unless tag_group.has_key?(parts[1])
+          tag_group[parts[1]]['region'] = parts[11]
+          tag_group[parts[1]]['public-address'] = parts[16]
+          tag_group[parts[1]]['private-address'] = parts[17]
+        elsif parts[0] == "TAG"
+          tag_group[parts[2]] = {} unless tag_group.has_key?(parts[2])
+          tag_group[parts[2]]['tags'] = {} unless tag_group[parts[2]].has_key?('tags')
+          tag_group[parts[2]]['tags'][parts[3]] = parts[4]
+        elsif parts[0] == "BLOCKDEVICE"
+        end
+      }
+      
+      if tag_group.keys().size() == 0
+        ""
+      else
+        Marshal.dump(tag_group)
+      end
+    else
+      ""
+    end
+  end
+end
